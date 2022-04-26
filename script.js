8ae03ef9a6e5f3c6f956e6e2ee89e4f0dc7cf602
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-04-26 08:11:30 +0000

    update code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const books = [
   {
+    id: 1,
     title: "The Power of Focus",
     author: "Mark Victor Hansen",
-    id: 1,
   },
 ];
 
@@ -33,30 +33,3 @@ function getBookListFromLocalStorage() {
 }
 
 renderBookList(getBookListFromLocalStorage());
-
-const addBookForm = document.querySelector("#add-book");
-addBookForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const title = event.target.querySelector("#title").value;
-  const author = event.target.querySelector("#author").value;
-  const bookList = getBookListFromLocalStorage();
-  const id = bookList.length + 1;
-  bookList.push({
-    title,
-    author,
-    id,
-  });
-  this.reset();
-  renderBookList(bookList);
-  saveBookToStorage(bookList);
-});
-
-bookListSection.addEventListener("click", (event) => {
-  if (event.target.classList.contains("remove")) {
-    const { id } = event.target.dataset;
-    const bookList = getBookListFromLocalStorage();
-    const bookListFiltered = bookList.filter((book) => book.id !== +id);
-    renderBookList(bookListFiltered);
-    saveBookToStorage(bookListFiltered);
-  }
-});
